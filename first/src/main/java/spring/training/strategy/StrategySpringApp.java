92e24f1e9443d699ca0e8113110dfedf5c3858b7
Victor Rentea <victorrentea@gmail.com>
2019-04-23 11:59:11 +0300

    SRP aplicat la nivel de clasa: disipi logica in multe clase mititele cu nume explicit, precis

--- first/src/main/java/spring/training/strategy/StrategySpringApp.java
+++ first/src/main/java/spring/training/strategy/StrategySpringApp.java
@@ -1,7 +1,5 @@
 package spring.training.strategy;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,12 +17,13 @@ public class StrategySpringApp implements CommandLineRunner {
 	
 	private ConfigProvider configProvider = new ConfigFileProvider(); 
 	
+	@Autowired
+	private CustomsService service;
 	// TODO [1] Break CustomsService logic into Strategies
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
 	public void run(String... args) throws Exception {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CH,100,100) = " + service.computeCustomsTax("CH", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -32,16 +31,48 @@ public class StrategySpringApp implements CommandLineRunner {
 		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
 	}
 }
-
+@Service
 class CustomsService {
+	@Autowired
+	private UKTaxComputer uk;
+	
+	@Autowired
+	private EUTaxComputer eu;
+	
+	@Autowired
+	private CHTaxComputer ch;
+	
 	public double computeCustomsTax(String originCountry, double tobacoValue, double regularValue) { // UGLY API we CANNOT change
 		switch (originCountry) { 
-		case "UK": return tobacoValue/2 + regularValue/2; 
-		case "CH": return tobacoValue + regularValue;
+		case "UK": return uk.computeUKTax(tobacoValue, regularValue); 
+		case "CH": return ch.computeCHTax(tobacoValue, regularValue);
 		case "FR": 
 		case "ES": // other EU country codes...
-		case "RO": return tobacoValue/3;
+		case "RO": return eu.computeEUTax(tobacoValue);
 		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
 		} 
+	}
+}
+@Service
+class W{ }
+@Service
+class EUTaxComputer {
+	@Autowired
+	private W w;
+	public double computeEUTax(double tobacoValue) {
+		System.out.println("w: " + w);
+		return tobacoValue/3;
+	}
+}
+@Service
+class CHTaxComputer {
+	public double computeCHTax(double tobacoValue, double regularValue) {
+		return tobacoValue + regularValue;
+	}
+}
+@Service
+class UKTaxComputer {
+	public double computeUKTax(double tobacoValue, double regularValue) {
+		return tobacoValue/2 + regularValue/2;
 	}
 }
