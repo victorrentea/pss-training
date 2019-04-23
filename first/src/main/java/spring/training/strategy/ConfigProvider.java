f0014f6c0d9ee68057c6248763e24bd084f8ddd9
Victor Rentea <victorrentea@gmail.com>
2019-04-23 11:46:54 +0300

    Spring Strategy™ Design Pattern exerise

---
+++ first/src/main/java/spring/training/strategy/ConfigProvider.java
@@ -0,0 +1,34 @@
+package spring.training.strategy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public interface ConfigProvider {
+	Properties getProperties();
+}
+
+class ConfigFileProvider implements ConfigProvider {
+	
+	public Properties getProperties() {
+		try (InputStream is = ConfigFileProvider.class.getResourceAsStream("/application.properties")) {
+			Properties properties = new Properties();
+			properties.load(is);
+			return properties; 
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
+class ConfigDatabaseProvider implements ConfigProvider {
+
+	public Properties getProperties() {
+		// Real implem goes here
+		Properties properties = new Properties();
+		properties.setProperty("someProp", "from database");
+		return properties; 
+	}
+	
+}
+
