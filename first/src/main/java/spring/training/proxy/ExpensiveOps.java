ebe234133b3e20337077d73736d079c83e0914a3
Victor Rentea <victorrentea@gmail.com>
2019-04-24 09:14:07 +0300

    Tema + rename proj + discutii la proxy

--- first/src/main/java/spring/training/proxy/ExpensiveOps.java
+++ first/src/main/java/spring/training/proxy/ExpensiveOps.java
@@ -14,6 +14,7 @@ import org.apache.commons.io.FileUtils;
 import org.jooq.lambda.Unchecked;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,8 @@ public class ExpensiveOps {
 	
 	@LoggedMethod
 	@Cacheable("primes")
+	// daca faci metodala finala sau privata Aspectele nu pot interveni.
+	// pentru ca nu merge @Override
 	public Boolean isPrime(int n) {
 		log.debug("Computing isPrime({})", n);
 		
@@ -49,9 +52,21 @@ public class ExpensiveOps {
 		return true;
 	}
 	
+//	@Transactional(REQUIRES_NEW)
+//	private void salveazaBatch(List<String> listaDe500) {
+//		
+//	}
+//	
+	
+	@Autowired
+	private ExpensiveOps euInOglinda;
+	
 	@Cacheable("folders")
 	public String hashAllFiles(File folder) {
 		log.debug("Computing hashAllFiles({})", folder);
+		
+		log.debug("Got: " + euInOglinda.isPrime(10_000_169) + "\n");
+		
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			for (int i = 0; i < 2; i++) { // pretend there is much more work to do here
