17a3066eb8396d217afb42be40d00d15e195b4b6
Victor Rentea <victorrentea@gmail.com>
2019-04-24 10:43:04 +0300

    Se deschide editul

--- pss-spring-spa/src/main/java/training/spring/web/CoursesController.java
+++ pss-spring-spa/src/main/java/training/spring/web/CoursesController.java
@@ -4,29 +4,41 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import training.spring.domain.Course;
 import training.spring.repo.CourseRepository;
 import training.spring.web.dto.CourseDto;
 
 @RestController
+@RequestMapping("/rest/courses")
 public class CoursesController {
 
 	@Autowired
 	private CourseRepository repo;
 	
-	@RequestMapping("/rest/courses")
+	@GetMapping
 	public List<CourseDto> getCourses() {
 		return repo.findAll().stream().map(CourseDto::new).collect(toList());
 	}
 	
-	@RequestMapping("/rest/courses/{id}")
+	@DeleteMapping("{id}")
 	public void deleteCourse(@PathVariable Long id) {
 		repo.deleteById(id);
 	}
+	
+	
+//	@RequestMapping(value = "courses/{id}", method = RequestMethod.GET)
+	@GetMapping("{id}")
+	public CourseDto getCourse(@PathVariable Long id) {
+		Course entity = repo.getById(id);
+		return new CourseDto(entity);
+	}
+	
+	
 }
---
+++ pss-spring-spa/src/main/java/training/spring/web/TeachersController.java
@@ -0,0 +1,5 @@
+package training.spring.web;
+
+public class TeachersController {
+
+}
