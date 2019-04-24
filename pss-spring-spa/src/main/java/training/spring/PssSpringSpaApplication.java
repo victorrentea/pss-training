f7b7389cd60fc1c8ebf044356b3b16d4087db439
Victor Rentea <victorrentea@gmail.com>
2019-04-24 15:19:07 +0300

    Scheduled

--- pss-spring-spa/src/main/java/training/spring/PssSpringSpaApplication.java
+++ pss-spring-spa/src/main/java/training/spring/PssSpringSpaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableScheduling
 @EnableSwagger2
 @SpringBootApplication
 public class PssSpringSpaApplication {
---
+++ pss-spring-spa/src/main/java/training/spring/SchedulerPlay.java
@@ -0,0 +1,27 @@
+package training.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerPlay {
+	private final static Logger log = LoggerFactory.getLogger(SchedulerPlay.class);
+
+	@Scheduled(cron = "0/5 * * * * *")
+	public void helloNarcis() throws InterruptedException {
+		log.debug("Ii spun acush...");
+		Thread.sleep(6000);
+		log.debug("Ce frumos esti!");
+	}
+	
+	
+	@Scheduled(cron = "0/5 * * * * *")
+	public void hater() throws InterruptedException {
+		log.debug("Ii spun acush...");
+		Thread.sleep(6000);
+		log.debug("Urat esti mah!");
+	}
+	
+}
