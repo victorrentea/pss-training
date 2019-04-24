15b971964d8c0aef0439502b0de8cb95aa90ef1d
Victor Rentea <victorrentea@gmail.com>
2019-04-24 16:01:35 +0300

    @Async!

--- pss-spring-spa/src/main/java/training/spring/web/AsyncPlaygroundController.java
+++ pss-spring-spa/src/main/java/training/spring/web/AsyncPlaygroundController.java
@@ -1,12 +1,14 @@
 package training.spring.web;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,39 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class AsyncPlaygroundController {
 	private final static Logger log = LoggerFactory.getLogger(AsyncPlaygroundController.class);
 	
-	public static final ExecutorService putz = Executors.newFixedThreadPool(2);
+	@Autowired
+	private Worker worker;
 	
 	@GetMapping("parallel")
 	public void fa2chestiiOdata() throws InterruptedException, ExecutionException {
-		log.info("Incep");
-		Future<Void> viitorNimic = putz.submit(() -> task1());
-		Future<Integer> viitorNumar = putz.submit(() -> task2());
+		log.info("Incep cu " + worker.getClass());
+		Future<Void> viitorNimic = worker.task1();
+		Future<Integer> viitorNumar = worker.task2();
 		
 		viitorNimic.get();
 		log.info("Numarul norocos este : " +viitorNumar.get());
 		log.info("Termin");
 	}
+	@Component
+	public static class Worker {
 	
-	public Void task1() {
-		log.info("start1");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+		@Async
+		public Future<Void> task1() {
+			log.info("start1");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+	//		if (true) throw new IllegalArgumentException(); // pune-o la loc ca sa vezi de ce intorc Void !
+			log.info("end1");
+			return CompletableFuture.completedFuture(null);
 		}
-//		if (true) throw new IllegalArgumentException(); // pune-o la loc ca sa vezi de ce intorc Void !
-		log.info("end1");
-		return null;
-	}
-	public int task2() {
-		log.info("start2");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+		@Async
+		public Future<Integer> task2() {
+			log.info("start2");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			log.info("end2");
+	
+			return CompletableFuture.completedFuture(6);
 		}
-		log.info("end2");
-
-		return 6;
 	}
 }
