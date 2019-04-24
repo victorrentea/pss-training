35e8570383cee8a0acdf17611205b82e0cf678b6
Victor Rentea <victorrentea@gmail.com>
2019-04-24 09:43:18 +0300

    Spring SPA skeleton

---
+++ pss-spring-spa/src/main/java/training/spring/pssspringspa/PssSpringSpaApplication.java
@@ -0,0 +1,13 @@
+package training.spring.pssspringspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PssSpringSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PssSpringSpaApplication.class, args);
+	}
+
+}
