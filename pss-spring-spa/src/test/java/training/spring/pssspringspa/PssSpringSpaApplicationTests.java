ca1e845ab8a94b53a61b5a63e291b3fad0f9f3d0
Victor Rentea <victorrentea@gmail.com>
2019-04-24 16:39:14 +0300

    @MockBean -- Pui springul pe picioare si ii mockuiesti doar cateva din beanuri

--- pss-spring-spa/src/test/java/training/spring/pssspringspa/PssSpringSpaApplicationTests.java
+++ pss-spring-spa/src/test/java/training/spring/pssspringspa/PssSpringSpaApplicationTests.java
@@ -1,16 +1,20 @@
 package training.spring.pssspringspa;
 
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.text.ParseException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import training.spring.domain.Course;
 import training.spring.domain.Teacher;
 import training.spring.repo.CourseRepository;
 import training.spring.repo.TeacherRepository;
@@ -20,6 +24,43 @@ import training.spring.web.dto.CourseDto;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PssSpringSpaApplicationTests {
+	
+	@Autowired
+	private CoursesController controller;
+	
+	@MockBean
+	private TeacherRepository teacherRepo;
+	@MockBean
+	private CourseRepository courseRepo;
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void createCourseFailsForMissingDescription() throws ParseException {
+		when(teacherRepo.getById(13L)).thenReturn(new Teacher());
+		CourseDto dto = new CourseDto();
+		dto.teacherId = 13L;
+		controller.createCourse(dto);
+	}
+	
+	@Test
+	public void createCourse() throws ParseException {
+		when(teacherRepo.getById(13L)).thenReturn(new Teacher());
+		CourseDto dto = new CourseDto();
+		dto.teacherId = 13L;
+		dto.description="d";
+		dto.startDate="2019-01-01";
+		controller.createCourse(dto);
+		
+		verify(courseRepo).save(ArgumentMatchers.any());
+	}
+	@Test(expected = ParseException.class)
+	public void createCourseFailsForDateforma() throws ParseException {
+		when(teacherRepo.getById(13L)).thenReturn(new Teacher());
+		CourseDto dto = new CourseDto();
+		dto.teacherId = 13L;
+		dto.description="d";
+		dto.startDate="2019-01321";
+		controller.createCourse(dto);
+	}
 
 
 }
