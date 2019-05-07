dcfc20836be47fe678a7b99ac792c919c218eea2
Victor Rentea <victorrentea@gmail.com>
2019-05-07 15:39:49 +0300

    Primu meu insertcu JPA

--- pss-jpa/src/main/java/ro/victor/training/jpa2/Playground.java
+++ pss-jpa/src/main/java/ro/victor/training/jpa2/Playground.java
@@ -1,12 +1,26 @@
 package ro.victor.training.jpa2;
 
+import java.util.Random;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import ro.victor.training.jpa2.domain.entity.ErrorLog;
 
 @Component
 public class Playground {
+	
+	@Autowired
+	private EntityManager em;
 
+	@Transactional
 	public void firstTransaction() {
-		 // TODO
+		em.persist(new ErrorLog("Defect Driven Development"));
+		
+		
 	}
 
 	public void secondTransaction() {
